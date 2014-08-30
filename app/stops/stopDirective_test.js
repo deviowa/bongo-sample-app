8172b918433f793698e8e1138e79c434ce439981
Riley Eynon-Lynch <riley@peardeck.com>
2014-08-30 17:31:52 -0500

    Improve test code for students

--- app/stops/stopDirective_test.js
+++ app/stops/stopDirective_test.js
@@ -3,10 +3,10 @@ describe('thing', function () {
     // we're fetching an injector from angular that knows about 'ng' and 'angularcourse'
     var inject = angular.injector(['ng', 'angularcourse']).invoke;
 
-    // this is a bit of near-boilerplate that simulates generating a bus-stop directive
-    function createStopDirective() {
+    // this is a bit of boilerplate to easily compile some markup
+    function compile(markup) {
         return inject(function ($compile, $rootScope) {
-            var element = $compile("<bus-stop></bus-stop>")($rootScope.$new());
+            var element = $compile(markup)($rootScope.$new());
             $rootScope.$digest();
             return element;
         });
@@ -14,7 +14,7 @@ describe('thing', function () {
 
     // this is the actual test
     it('compiles and displays "Bus stop"', function () {
-        var busStopDirective = createStopDirective();
+        var busStopDirective = compile("<bus-stop></bus-stop>");
 
         expect(busStopDirective.html()).toContain("Title of Stop");
     });
