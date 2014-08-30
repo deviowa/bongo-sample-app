324857d736ff3061f5fc8bac6a4bc1c50394beaa
Riley Eynon-Lynch <riley@peardeck.com>
2014-08-30 13:49:59 -0500

    Cleanup

--- build/app_controller_test.js
+++
@@ -1,5 +0,0 @@
-describe('app_controller_test', function() {
-  return it('should assert something', function() {
-    return expect(true).toBe(true);
-  });
-});
--- build/templates.js
+++
@@ -1,3 +0,0 @@
-angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("mainMenu/mainMenu.html","Main menu!");
-$templateCache.put("about.html","<h1>About</h1><p>Written by<span id=\"author-name\">Riley Eynon-Lynch</span></p>");
-$templateCache.put("home.html","<h1 id=\"app-title\">Amazing Bus Route Navigator!</h1>");}]);
