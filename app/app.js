318f7fb746916c554ef4c2944ed113d1a0b5d227
Riley Eynon-Lynch <riley@peardeck.com>
2014-08-30 15:06:34 -0500

    LESSON 1 START

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
-var mainMenuModule = require('./mainMenu/module');
 var navigationModule = require('./navigation/module');
 
-var app = angular.module('angularcourse', ['templates', navigationModule.name, mainMenuModule.name]);
+var app = angular.module('angularcourse', ['templates', navigationModule.name]);
 
