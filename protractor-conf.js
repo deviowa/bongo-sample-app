5b1722e52ca3538601f1a7054af0a0a27c8fb491
Riley Eynon-Lynch <riley@peardeck.com>
2014-09-13 16:27:11 -0500

    Don't require webdriver-manager when running tests

--- protractor-conf.js
+++ protractor-conf.js
@@ -2,6 +2,7 @@ require('coffee-script/register');
 
 // conf.js
 exports.config = {
+  chromeOnly: true,
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['**/*.e2e.js', '**/*.e2e.coffee']
 };
